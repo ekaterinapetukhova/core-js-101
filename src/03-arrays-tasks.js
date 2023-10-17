36a9c7bab981fea5c70bf6f649abf4c953585670
Ekaterina <katepthv12@gmail.com>
2023-10-17 15:25:26 +0200

    feat: solve getMovingSum task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -233,12 +233,16 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let sum = 0;
+  return arr.map((item) => {
+    sum += item;
+    return sum;
+  });
 }
 
 /**
- * Returns every second item from the specified array:
+ * Returns every second item from the specbified array:
  *
  * @param {array} arr
  * @return {array}
