157222ec3435d88906626542599a2ef364889f79
Ekaterina <katepthv12@gmail.com>
2023-10-19 13:45:43 +0200

    feat: solve distinct task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -519,8 +519,17 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const uniqArr = [];
+
+  arr.map((item) => {
+    if (!uniqArr.includes(item)) {
+      uniqArr.push(item);
+    }
+    return item;
+  });
+
+  return uniqArr;
 }
 
 /**
